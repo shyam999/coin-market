882032b55473181f2b716e10f7e4b06a6e0a89ae
shyam999 <Shyam.acharjya.1@gmail.com>
2022-09-22 14:50:34 +0530

    added template and url for coinapp

--- coinmarket/urls.py
+++ coinmarket/urls.py
@@ -1,8 +1,9 @@
 """coinmarket URL Configuration
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('coinapp.urls'))
 ]
