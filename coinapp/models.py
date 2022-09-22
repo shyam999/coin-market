d9189b09df4f60e3dce9ec5c30651e40d7308371
shyam999 <Shyam.acharjya.1@gmail.com>
2022-09-22 14:59:00 +0530

    added models to the coinapp

--- coinapp/models.py
+++ coinapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50, blank=True)
+    time_1h = models.CharField(max_length=50, blank=True)
+    time_24h = models.CharField(max_length=50, blank=True)
+    time_7d = models.CharField(max_length=50, blank=True)
+    market_cap = models.CharField(max_length=50, blank=True)
+    Volume_24h = models.CharField(max_length=50, blank=True)
+    circulating_supply = models.CharField(max_length=50, blank=True)
+    
+    def __str__(self):
+        return self.name
